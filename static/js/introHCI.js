937e55d2bca4a912dd23393f57045c3d81bfa5e6
vagrant <vagrant@precise32.(none)>
2014-01-23 18:38:24 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -14,9 +14,7 @@ function initializePage() {
 		$("#testjs").text("Please wait...");
 		$(".jumbotron p").toggleClass("active");
 	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("#submitBtn").click(updateProject);
 	$("a.thumbnail").click(projectClick);
 }
 
@@ -30,25 +28,20 @@ function projectClick(e) {
 	var containingProject = $(this).closest(".project");
 	var description = $(containingProject).find(".project-description");
 	if (description.length == 0) {
-		$(containingProject).append("div class='project-description'><p>Description of the project.</p></div>");
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
 	} else {
-		description.html("<p>Stop clicking on me! You just did it at " + (
-			new Date()) + "</p>");
+		//description.html("<p>Stop clicking on me! You just did it at " + (
+			//new Date()) + "</p>");
+		$(description).fadeOut();
 	}
-
-	//$("#containingProject").fadeOut();
 }
 
-	function initializePage() {
-		$("#submitBtn").click(updateProject);
-	}
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
 
-	function updateProject(e) {
-		var projectID = $('#project').val();
-		$(projectID).animate({
-			width: $('#width').val()
-	});
-
-		var newText = $('description').val();
-		$(projectID + ".project-description").text(newText);
-	}
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
